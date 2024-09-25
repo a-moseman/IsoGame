8eff4784448beb4767dc618fe3e052c20d24b8f7
a-moseman <drewmoseman@gmail.com>
2024-09-25 16:46:28 -0400

    Transition from 3D to 2D

--- src/node/Game.gd
+++ src/node/Game.gd
@@ -4,7 +4,7 @@ const Bootstrap = preload("res://src/boot/Bootstrap.gd")
 const InstanceType = Bootstrap.InstanceType
 
 @onready
-var player_prefab = preload("res://player.tscn")
+var player_prefab = preload("res://prefab/player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +12,10 @@ func _ready():
 	var instance_type: InstanceType = Bootstrap.new().run(args, self)
 	if multiplayer.is_server():
 		multiplayer.peer_connected.connect(add_player)
+		
 		# add already connected clients
-		for id in multiplayer.get_peers():
-			add_player(id)
+		#for id in multiplayer.get_peers():
+		#	add_player(id)
 	
 func add_player(id: int):
 	var player = player_prefab.instantiate()
