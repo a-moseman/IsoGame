de4add774277f3a1c225737a3b5c5b9e01286717
a-moseman <drewmoseman@gmail.com>
2024-10-02 08:34:16 -0400

    Add Entity class

---
+++ src/model/Entity.gd
@@ -0,0 +1,22 @@
+class_name Entity extends Object
+
+# imports
+const BoundedInt = preload("res://src/model/BoundedInt.gd")
+
+## The name of the entity.
+var _name: String
+## The health of the entity.
+var _health: BoundedInt
+
+## Instantiate an entity.
+func _init(name: String, health: BoundedInt):
+	self._name = name
+	self._health = health
+	
+## Get the name of the entity.
+func name() -> String:
+	return self._name
+	
+## Get the health of the entity.
+func health() -> BoundedInt:
+	return self._health
